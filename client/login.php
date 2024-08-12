02a4fc1b879f905fd9596a0b9778c6236a311d3d
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-08-12 21:13:05 +0530

    worked on login and signup ui

---
+++ client/login.php
@@ -0,0 +1,23 @@
+<div class="container">
+<h1 class="heading">Login </h1>
+
+<form>
+
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="email" class="form-label">User Email</label>
+    <input type="text" class="form-control" id="email" placeholder="enter user email">
+  </div>
+
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+    <label for="password" class="form-label">User Password</label>
+    <input type="password" class="form-control" id="password" placeholder="enter user password">
+  </div>
+  <div class="col-6 offset-sm-3 margin-bottom-15">
+  <button type="submit" class="btn btn-primary">Login</button>
+
+  </div>
+
+</form>
+
+</div>
