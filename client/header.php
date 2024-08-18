5f3072c9c18d28f56cc303f06c0e20625029ffcf
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2024-08-18 17:20:11 +0530

    done with search

--- client/header.php
+++ client/header.php
@@ -41,6 +41,10 @@
           <a class="nav-link" href="?latest=true">Latest Questions</a>
         </li>
       </ul>
+      <form class="d-flex" action="">
+        <input class="form-control me-2" name="search" type="search" placeholder="Search questions">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
   </div>
 </nav>
